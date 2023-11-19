dd46ab9d6d0ab79a6a1f73fd3581a263b12d7895
Jay Trivedi <jayutrivedi1214@gmail.com>
2023-11-19 18:18:15 +0530

    Update Code.java

--- CODE/2048 Game/Code.java
+++ CODE/2048 Game/Code.java
@@ -22,7 +22,7 @@ public class Code_2048 {
         random = new Random();
         score = 0;
         highScore = 0;
-        frame = new JFrame("Malvika's 2048");
+        frame = new JFrame("Jay's 2048");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(400, 450);
         frame.setLayout(new BorderLayout());
